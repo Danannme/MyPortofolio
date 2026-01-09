c0aa0da016794c602b7e305fe0eed1dbb3c4c1e5
Danannme <noragamiaragotoyato92@gmail.com>
2026-01-09 22:05:26 +0700

    Update: - add 2 more project (travelo & webncraft) - change navbar background style

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,150 +1,143 @@
 import { useState, useEffect } from "react";
 import {
-    Navbar,
-    MobileNav,
-    Typography,
-    IconButton,
+  Navbar,
+  MobileNav,
+  Typography,
+  IconButton,
 } from "@material-tailwind/react";
 
 export default function StickyNavbar({ openNav, setOpenNav, navIconClicked }) {
-    useEffect(() => {
-        window.addEventListener(
-            "resize",
-            () => window.innerWidth >= 960 && setOpenNav(false)
-        );
-    }, []);
-
-    const navList = (
-        <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-            <Typography
-                as="li"
-                variant="small"
-                color="black"
-                className="p-1 font-normal"
-            >
-                <a
-                    href="#"
-                    className="flex items-center"
-                    onClick={navIconClicked}
-                >
-                    HOME
-                </a>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="black"
-                className="p-1 font-normal"
-            >
-                <a
-                    href="#about"
-                    className="flex items-center"
-                    onClick={navIconClicked}
-                >
-                    ABOUT
-                </a>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="black"
-                className="p-1 font-normal"
-            >
-                <a
-                    href="#skills"
-                    className="flex items-center"
-                    onClick={navIconClicked}
-                >
-                    SKILLS
-                </a>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="black"
-                className="p-1 font-normal"
-            >
-                <a
-                    href="#portofolio"
-                    className="flex items-center"
-                    onClick={navIconClicked}
-                >
-                    PORTOFOLIO
-                </a>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="black"
-                className="p-1 font-normal"
-            >
-                <a
-                    href="#contact"
-                    className="flex items-center"
-                    onClick={navIconClicked}
-                >
-                    CONTACT
-                </a>
-            </Typography>
-        </ul>
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false),
     );
+  }, []);
 
-    return (
-        <div className="sticky top-0 max-h-[768px] w-[100%] z-10">
-            <Navbar className="bg-transparent border-none shadow-none h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
-                <div className="flex items-center justify-between text-black">
-                    <Typography
-                        as="a"
-                        href="#"
-                        className="mr-4 cursor-pointer py-1.5 font-medium text-xl"
-                    >
-                        Robbi Zaidan A
-                    </Typography>
-                    <div className="flex items-center gap-4">
-                        <div className="mr-4 hidden lg:block">{navList}</div>
-                        <IconButton
-                            variant="text"
-                            className="ml-auto h-6 w-6 text-inherit cursor-pointer hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-                            ripple={false}
-                            onClick={() => setOpenNav(!openNav)}
-                        >
-                            {openNav ? (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    className="h-6 w-6"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            ) : (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-6 w-6"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                </svg>
-                            )}
-                        </IconButton>
-                    </div>
-                </div>
-                <MobileNav open={openNav} className="">
-                    {navList}
-                </MobileNav>
-            </Navbar>
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="black"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center" onClick={navIconClicked}>
+          HOME
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="black"
+        className="p-1 font-normal"
+      >
+        <a href="#about" className="flex items-center" onClick={navIconClicked}>
+          ABOUT
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="black"
+        className="p-1 font-normal"
+      >
+        <a
+          href="#skills"
+          className="flex items-center"
+          onClick={navIconClicked}
+        >
+          SKILLS
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="black"
+        className="p-1 font-normal"
+      >
+        <a
+          href="#portofolio"
+          className="flex items-center"
+          onClick={navIconClicked}
+        >
+          PORTOFOLIO
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="black"
+        className="p-1 font-normal"
+      >
+        <a
+          href="#contact"
+          className="flex items-center"
+          onClick={navIconClicked}
+        >
+          CONTACT
+        </a>
+      </Typography>
+    </ul>
+  );
+
+  return (
+    <div className="sticky top-0 max-h-[768px] w-[100%] z-10">
+      <Navbar className="border-none shadow-none h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+        <div className="flex items-center justify-between text-black">
+          <Typography
+            as="a"
+            href="#"
+            className="mr-4 cursor-pointer py-1.5 font-medium text-xl"
+          >
+            RZA
+          </Typography>{" "}
+          <div className="flex items-center gap-4">
+            {" "}
+            <div className="mr-4 hidden lg:block">{navList}</div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit cursor-pointer hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
         </div>
-    );
+        <MobileNav open={openNav} className="">
+          {navList}
+        </MobileNav>
+      </Navbar>
+    </div>
+  );
 }
--- src/Components/Portofolio.jsx
+++ src/Components/Portofolio.jsx
@@ -3,111 +3,125 @@ import { useState } from "react";
 import CardPorto from "../Elements/CardPorto";
 
 import {
-    Tabs,
-    TabsHeader,
-    TabsBody,
-    Tab,
-    TabPanel,
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
 } from "@material-tailwind/react";
 
 const Portofolio = () => {
-    const [isActive, setIsActive] = useState("project");
+  const [isActive, setIsActive] = useState("project");
 
-    const data = [
-        {
-            label: "Project",
-            value: "project",
-            element: [
-                <CardPorto
-                    imgSrc={"/img/project image/websiteKelas10.webp"}
-                    title={"Website Kelas 10"}
-                    desc={
-                        "I created this website when I was in 10th grade of high school, to publish about my class."
-                    }
-                    techs={["Html", "Css", "Javascript"]}
-                    linkTo={"https://profilex-gclass.pages.dev"}
-                    btnText={"Visit"}
-                />,
-                <CardPorto
-                    imgSrc={"/img/project image/websiteKelas11.webp"}
-                    title={"Website Kelas 11"}
-                    desc={
-                        "I created this website when I was in 11th grade of high school, to publish about my class. There are improvements in the technology I use compared to the previous website."
-                    }
-                    techs={[
-                        "Html",
-                        "Css",
-                        "Javascript",
-                        "Tailwind",
-                        "Bootstrap Icon",
-                    ]}
-                    linkTo={"https://thegrotesque.my.id"}
-                    btnText={"Visit"}
-                />,
-                <CardPorto
-                    imgSrc={"/img/project image/noteAppRaect.webp"}
-                    title={"React Note App"}
-                    desc={
-                        "I created this Note App using React js with simple programming, and have implemented a search note."
-                    }
-                    techs={["Html", "Css", "Javascript", "React Js"]}
-                    linkTo={"https://note-app-snowy-mu.vercel.app"}
-                    btnText={"Visit"}
-                />,
-            ],
-        },
-        {
-            label: "Certificate",
-            value: "certificate",
-            element: [
-                <CardPorto
-                    imgSrc={"/img/project image/sertifikatReact.webp"}
-                    title={"Sertifikat React Js"}
-                    desc={
-                        "I got this certificate on December 11, 2024 after completing the React Js training at Dicoding."
-                    }
-                    techs={[]}
-                    linkTo={
-                        "https://drive.google.com/file/d/1gDIzgi-vzk5jGHX8qwtxXY3s3LsSlsqT/view?usp=sharing"
-                    }
-                    btnText={"See"}
-                />,
-            ],
-        },
-    ];
+  const data = [
+    {
+      label: "Project",
+      value: "project",
+      element: [
+        <CardPorto
+          imgSrc={"/img/project image/websiteKelas10.webp"}
+          title={"Website Kelas 10"}
+          desc={
+            "I created this website when I was in 10th grade of high school, to publish about my class."
+          }
+          techs={["Html", "Css", "Javascript"]}
+          linkTo={"https://profilex-gclass.pages.dev"}
+          btnText={"Visit"}
+        />,
+        <CardPorto
+          imgSrc={"/img/project image/websiteKelas11.webp"}
+          title={"Website Kelas 11"}
+          desc={
+            "I created this website when I was in 11th grade of high school, to publish about my class. There are improvements in the technology I use compared to the previous website."
+          }
+          techs={["Html", "Css", "Javascript", "Tailwind", "Bootstrap Icon"]}
+          linkTo={"https://thegrotesque.my.id"}
+          btnText={"Visit"}
+        />,
+        <CardPorto
+          imgSrc={"/img/project image/noteAppRaect.webp"}
+          title={"React Note App"}
+          desc={
+            "I created this Note App using React js with simple programming, and have implemented a search note."
+          }
+          techs={["Html", "Css", "Javascript", "React Js"]}
+          linkTo={"https://note-app-snowy-mu.vercel.app"}
+          btnText={"Visit"}
+        />,
+        <CardPorto
+          imgSrc={"/img/project image/websiteTemplate-travelo.webp"}
+          title={"Travelo"}
+          desc={
+            "This is a landing page template about travel website called Travelo"
+          }
+          techs={["React Js", "Tailwindcss", "DaisyUi"]}
+          linkTo={"https://travelo-day.vercel.app"}
+          btnText={"Visit"}
+        />,
+        <CardPorto
+          imgSrc={"/img/project image/websiteTemplate-wnc.webp"}
+          title={"WebNCraft"}
+          desc={
+            "This is a landing page template about website builder agency called WebNCraft"
+          }
+          techs={["React Js", "Tailwindcss", "Shadcn", "Framer Motion"]}
+          linkTo={"https://webncraft-portfolio.vercel.app/"}
+          btnText={"Visit"}
+        />,
+      ],
+    },
+    {
+      label: "Certificate",
+      value: "certificate",
+      element: [
+        <CardPorto
+          imgSrc={"/img/project image/sertifikatReact.webp"}
+          title={"Sertifikat React Js"}
+          desc={
+            "I got this certificate on December 11, 2024 after completing the React Js training at Dicoding."
+          }
+          techs={[]}
+          linkTo={
+            "https://drive.google.com/file/d/1gDIzgi-vzk5jGHX8qwtxXY3s3LsSlsqT/view?usp=sharing"
+          }
+          btnText={"See"}
+        />,
+      ],
+    },
+  ];
 
-    return (
-        <div className="portofolio grid gap-4 w-full">
-            <h1
-                className="title text-3xl text-center font-medium before:left-[58px] before:w-[145px] scroll-mt-60 lg:scroll-mt-14"
-                id="portofolio"
-            >
-                Portofolio
-            </h1>
-            <div className="porto-body w-full max-w-[80rem] mx-auto">
-                <Tabs value="project">
-                    <TabsHeader className="bg-gray-300 w-55 max-w-[400px] h-10 mx-auto z-0">
-                        {data.map(({ label, value }) => (
-                            <Tab key={value} value={value}>
-                                {label}
-                            </Tab>
-                        ))}
-                    </TabsHeader>
-                    <TabsBody>
-                        {data.map(({ value, element }) => (
-                            <TabPanel
-                                key={value}
-                                value={value}
-                                className="flex flex-wrap justify-center mx-auto gap-4"
-                            >
-                                {element}
-                            </TabPanel>
-                        ))}
-                    </TabsBody>
-                </Tabs>
-            </div>
-        </div>
-    );
+  return (
+    <div className="portofolio grid gap-4 w-full">
+      <h1
+        className="title text-3xl text-center font-medium before:left-[58px] before:w-[145px] scroll-mt-60 lg:scroll-mt-14"
+        id="portofolio"
+      >
+        Portofolio
+      </h1>
+      <div className="porto-body w-full max-w-[80rem] mx-auto">
+        <Tabs value="project">
+          <TabsHeader className="bg-gray-300 w-55 max-w-[400px] h-10 mx-auto z-0">
+            {data.map(({ label, value }) => (
+              <Tab key={value} value={value}>
+                {label}
+              </Tab>
+            ))}
+          </TabsHeader>
+          <TabsBody>
+            {data.map(({ value, element }) => (
+              <TabPanel
+                key={value}
+                value={value}
+                className="flex flex-wrap justify-center mx-auto gap-4"
+              >
+                {element}
+              </TabPanel>
+            ))}
+          </TabsBody>
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export default Portofolio;
